b6c93634ce15ea44f0dd9b4e01043099cad88587
DeepthiV <deepthiv@Admins-MacBook-Pro.local>
2022-08-21 15:39:48 +0530

    Storing GIF data

--- tests/gifportal.js
+++ tests/gifportal.js
@@ -2,10 +2,37 @@ const anchor = require("@project-serum/anchor");
 
 const main = async () => {
     console.log("Starting tests");
-    anchor.setProvider(anchor.Provider.env());
+
+    const provider = anchor.Provider.env();
+    anchor.setProvider(provider);
     const program = anchor.workspace.GifPortal;
-    const tx = await program.rpc.startStuffOff();
-    console.log("Ypur transaction signature", tx);
+
+    const baseAccount = anchor.web3.Keypair.generate()
+    const tx = await program.rpc.startStuffOff({
+        accounts:{
+            baseAccount: baseAccount.publicKey,
+            user: provider.wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [baseAccount]
+    });
+    console.log("Your transaction signature", tx);
+
+    let account = await program.account.baseAccount.fetch(
+        baseAccount.publicKey
+    );
+    console.log("GIF Count", account.totalGifs.toString());
+
+    await program.rpc.addGif("https://tenor.com/view/triforce-heroes-link-zelda-the-legend-of-zelda-yay-gif-22678972",{
+        accounts:{
+            baseAccount: baseAccount.publicKey,
+            user: provider.wallet.publicKey,
+        },
+    });
+
+    account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("GIF Count", account.totalGifs.toString());
+    console.log("GIF List", account.giflist);
 };
 
 const runMain = async () => {
